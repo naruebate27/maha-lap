85bc74c42503f0b789280351bbf8c361bab789f7
naruebate27 <naruebate27@gmail.com>
2019-09-22 10:12:04 +0700

    Add test

---
+++ maha-lap.test.js
@@ -0,0 +1,8 @@
+const MahaLap = require("./maha-lap");
+
+describe("Test Maha-Lap", () => {
+  test("Test Tamnai with day", () => {
+    const result = MahaLap.tamnaiWithDay("จันทร์");
+    expect(result).toEqual(2);
+  });
+});
